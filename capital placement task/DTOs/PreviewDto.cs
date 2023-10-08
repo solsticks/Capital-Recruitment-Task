d801cda20f8ba378f0e328264fa22c37a3da4329
Olusola Adekan <olusola.adekan@azenia.com>
2023-10-08 11:36:16 +0100

    added prview model and DTO

---
+++ capital placement task/DTOs/PreviewDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capital_placement_task.DTOs
+{
+    public class PreviewDto
+    {
+        public string? LondonIntershipProg { get; set; }
+        public string? ProgramBenefits { get; set; }
+        public String? ApplicationCriteria { get; set; }
+    }
+}
---
+++ capital placement task/Models/Preview.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capital_placement_task.Models
+{
+    public class Preview
+    {
+        public string Id { get; set; }
+        public string? LondonIntershipProg { get; set; }
+        public string? ProgramBenefits { get; set; }
+        public string? ApplicationCriteria { get; set; }
+    }
+}
